e34f57ecd501a969ee48fffd8d32bce123789602
Parth Khokhar <115966725+parthkhokhar25@users.noreply.github.com>
2025-04-14 14:55:02 +0530

    Update insert.php

--- crud operation/insert.php
+++ crud operation/insert.php
@@ -1,27 +1,23 @@
 <?php
 include 'config.php';
-include 'form.php';
 
 $name = $_POST['name'];
 $email = $_POST['email'];
 $number = $_POST['number'];
 $date = date('y-m-d',strtotime($_POST['date']));
-$gender = $_POST['gender'];
 $city = $_POST['city'];
+$gender = $_POST['gender'];
 $course = $_POST['course'];
 $hobbies = $_POST['hobbies'];
 $hobbie = implode(",",$hobbies);     // implode function is used to contvert array to string
 
-
-
-$insert = "INSERT INTO crud (name,email,number,dob,gender,city,course,hobbies) VALUES ('$name','$email','$number','$date','$gender','$city','$course','$hobbie')";
+$insert = "INSERT INTO crud (name,email,number,dob,city,gender,course,hobbies) VALUES ('$name','$email','$number','$date','$city','$gender','$course','$hobbie')";
 
 $res = mysqli_query($con, $insert);
 
-header("location:view.php");
-
 if ($res == 1) {
-    echo "Record inserted";
+    //echo "Record inserted";
+    header("location:view.php");
 } else {
     echo "record not inserted";
 }
