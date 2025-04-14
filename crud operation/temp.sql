28d7db65c1b265cae4b39358480f5d4f2f95eaa7
Parth Khokhar <115966725+parthkhokhar25@users.noreply.github.com>
2025-04-14 14:58:01 +0530

    Update temp.sql

--- crud operation/temp.sql
+++ crud operation/temp.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jul 17, 2023 at 11:34 PM
--- Server version: 10.4.28-MariaDB
--- PHP Version: 8.2.4
+-- Generation Time: Apr 14, 2025 at 11:26 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -33,16 +33,24 @@ CREATE TABLE `crud` (
   `email` varchar(100) NOT NULL,
   `number` varchar(10) NOT NULL,
   `dob` date NOT NULL,
-  `gender` varchar(10) NOT NULL,
-  `city` varchar(100) NOT NULL
+  `city` varchar(100) NOT NULL,
+  `gender` varchar(100) NOT NULL,
+  `course` varchar(100) NOT NULL,
+  `hobbies` varchar(100) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `crud`
 --
 
-INSERT INTO `crud` (`id`, `name`, `email`, `number`, `dob`, `gender`, `city`) VALUES
-(1, 'asdfg', 'as@gmail.com', '2323456754', '2023-07-03', 'male', 'asdf');
+INSERT INTO `crud` (`id`, `name`, `email`, `number`, `dob`, `city`, `gender`, `course`, `hobbies`) VALUES
+(1, 'asdfg', 'as@gmail.com', '2323456754', '2023-07-03', 'asdf', '', '', ''),
+(2, 'temp', 'temp112@gmail.com', '1234567890', '2025-04-03', 'Rajkot', 'male', 'bba', 'music,cricket,reading'),
+(4, 'temp', 'temp32@gmail.com', '1234567890', '2025-04-24', 'rajkot', 'male', 'bba', 'music'),
+(5, 'temp2', 'temp2@gmail.com', '1234567899', '2025-04-07', 'rajkot', 'female', 'bca', 'music'),
+(6, 'temp', 'temp111@gmail.com', '1234567890', '2025-04-15', 'rajkot', 'male', 'bba', 'music'),
+(7, 'temp', 'temp111@gmail.com', '1234567890', '2025-04-16', 'rajkot', 'male', 'bba', 'music,cricket'),
+(8, 'temp', 'temp112@gmail.com', '1234567890', '2025-04-19', 'rajkot', 'male', 'bba', 'music');
 
 --
 -- Indexes for dumped tables
@@ -62,7 +70,7 @@ ALTER TABLE `crud`
 -- AUTO_INCREMENT for table `crud`
 --
 ALTER TABLE `crud`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
